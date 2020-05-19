60b5cd65e03b08686e9f2d46d869221ce739ed6d
Ned Wilde <ned.wilde@nike.com>
2020-05-18 21:13:20 -0700

    Correct header

--- pig.c
+++ pig.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include <pigpio.h>
 
 void _cb(int gpio, int level, uint32_t tick, void *user){
@@ -9,6 +9,6 @@ int main( int argc, char **argv ) {
 	if (gpioInitialise() < 0) return 1;
 	gpioSetMode(4, PI_INPUT);
 	gpioSetWatchdog(4, 10);
-  gpioSetAlertFuncEx(4, _cb, null);
+  gpioSetAlertFuncEx(4, _cb, 0);
   gpioTerminate();
 }
