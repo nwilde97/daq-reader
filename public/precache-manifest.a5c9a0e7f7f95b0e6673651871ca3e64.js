6994260a2bc76b0512f4105da47a16b118ece5f8
Ned Wilde <ned.wilde@nike.com>
2020-07-05 21:55:26 -0700

    Adding client and server

---
+++ public/precache-manifest.a5c9a0e7f7f95b0e6673651871ca3e64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f82dab8c890e4eb463c5edeaaa55d00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0b68e349d07a26bbbdf",
+    "url": "/static/css/main.9a55acce.chunk.css"
+  },
+  {
+    "revision": "aae8bdee0b06dd7ed851",
+    "url": "/static/js/2.441fc71e.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.441fc71e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0b68e349d07a26bbbdf",
+    "url": "/static/js/main.d056d5f2.chunk.js"
+  },
+  {
+    "revision": "3cb015972f7e045032d6",
+    "url": "/static/js/runtime-main.f1bc8154.js"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,45 @@
+"use strict";
+const open = require("open");
+const daq = require('./build/Release/module');
+process.on('exit', (code) => {
+    console.log(`Shutting down DAQ`, daq.shutdownDAQ());
+});
+(async () => {
+    //Setup express
+    let express = require('express');
+    const app = express();
+    const server = require('http').createServer(app);
+    app.use("/", express.static('public/index.html'));
+    app.use(express.static('public'));
+    //Setup Server
+    const io = require('socket.io')(server);
+    io.on('connection', client => {
+        // console.log("Client connected");
+        // client.on('disconnect', () => console.log("Client disconnected"));
+    });
+
+
+    //Setup DAQ Listener
+    const DEVICE_INDEX = daq.setupDAQ();
+    if(DEVICE_INDEX < 0){
+        console.log("No devices configured");
+    } else {
+        let i = 0;
+        const LOOP = setInterval(() => {
+            const voltages = daq.scanChannels(DEVICE_INDEX);
+            io.emit("scan", {voltages, timestamp: Date.now()});
+        }, 3);
+    }
+    // setInterval(() => {
+    //     const voltages = [];
+    //     for (let i = 0; i < 64; ++i) {
+    //         voltages.push(Math.random() * 10);
+    //     }
+    //     io.emit("scan", {voltages, timestamp: Date.now()});
+    // }, 50);
+
+    //Start the server
+    server.listen(8080);
+    console.log("The server is now running. You can visit it by visiting\nhttp://localhost:8080");
+    await open("http://localhost:8080");
+})();
