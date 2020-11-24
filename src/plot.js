af80d3e78937aa040de8f88f6fbd758f9292c506
nwilde97 <b.nwilde@gmail.com>
2020-11-23 18:30:32 -0800

    Plot with gnuplot

--- src/plot.js
+++ src/plot.js
@@ -8,15 +8,15 @@ process.on('exit', (code) => {
     console.log("Done", code);
 });
 (async () => {
-    const CHANNEL = parseInt(process.argv[2])-1;
-    if(CHANNEL < 0 || CHANNEL > 63){
+    const CHANNEL = parseInt(process.argv[2]) - 1;
+    if (CHANNEL < 0 || CHANNEL > 63) {
         console.log(`Invalid channel ${process.argv[2]}`);
         process.exit(1);
     }
 
     let data = [];
     data[999] = 0;
-    data.fill(0,0,1000);
+    data.fill(0, 0, 1000);
 
     const plot = gnuplot();
     plot.set("xrange [0:1000]");
@@ -27,24 +27,23 @@ process.on('exit', (code) => {
 
     //Setup DAQ Listener
     const DEVICE_INDEX = daq.setupDAQ();
-    if(DEVICE_INDEX < 0){
+    if (DEVICE_INDEX < 0) {
         console.log("No devices configured");
         plot.end();
     } else {
         let i = 0;
         const LOOP = setInterval(async () => {
             try {
-const voltages = daq.scanChannels(DEVICE_INDEX);
-            const x = voltages[CHANNEL];
-            data = data.slice(1);
-            data.push(x);
-            const out = data.map((val, idx) => `${idx}\t${val}`).join(EOL);
-            await fs.writeFile("data.out", out);
-console.log(x);
-            plot.println("replot");
-} catch(e){
-  process.exit(1);
-}
+                const voltages = daq.scanChannels(DEVICE_INDEX);
+                const x = voltages[CHANNEL];
+                data = data.slice(1);
+                data.push(x);
+                const out = data.map((val, idx) => `${idx}\t${val}`).join(EOL);
+                await fs.writeFile("data.out", out);
+                plot.println("replot");
+            } catch (e) {
+                process.exit(1);
+            }
         }, 50);
     }
 })().catch(err => console.log(err));
